a937481e0c373876cd974444df4be4eebaa53054
gamer-emus <ayaan1209sharma@gmail.com>
2025-11-27 19:27:06 +1100

    Add files via upload

---
+++ class6.py
@@ -0,0 +1,45 @@
+import cv2
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcasscade_frontalface_default.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+
+if not cap.isOpened():
+    print("Error: could not open camera")
+    exit()
+
+
+while True:
+    ret, frame = cap.read()
+
+
+    if not ret:
+        print("Error : image failed to capture")
+        break
+
+
+    gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
+
+
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor= 1.1 , minNeighbors=5, minSize=(30,30))
+
+
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame , (x,y ) , (x+w,y+h ) , (0,0,255),2)
+
+
+
+    cv2.imshow("Face detection - press q to quit the program " , frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
